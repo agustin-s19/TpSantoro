64ce6c5bb02d57f4c6f31e8b9037575f39d4a29d
agustin-s19 <agustin_sabia@hotmail.com>
2019-10-31 15:39:37 -0300

    Login

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            login();
+
+
+        }
+        static void login()
+        {
+            string contraseña;
+            string usuario;
+            Console.WriteLine("Usuario: ");
+            usuario = Console.ReadLine();
+            Console.WriteLine("CONTRASEÑA: ");
+            contraseña = Console.ReadLine();
+            Console.Clear();
+
+            while (usuario == "" && contraseña == "")
+            {
+                Console.WriteLine("Los datos que ingresaste son inválidos.\nPor favor ingrese bien sus datos \n");
+
+                login();
+                break;
+            }
+
+            if (usuario != "" && contraseña != "")
+
+
+
+                Console.WriteLine("Bienvenido" + usuario + "\nIngresando a la tienda");
+               
+
+        }
+    }
+}
